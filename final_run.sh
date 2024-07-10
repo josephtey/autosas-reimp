ddb1418333b2388d37bf27a0c640e70e687d0a39
Joseph Tey <jjttey@gmail.com>
2024-07-10 01:35:16 -0700

    final

---
+++ final_run.sh
@@ -0,0 +1,30 @@
+python3.9 train_auto_sas_.py --essay_set 1 --input_data_file 'data/asap_01/df_spell.csv' --input_splits_file 'data/asap_01/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 2 --input_data_file 'data/asap_02/df_spell.csv' --input_splits_file 'data/asap_02/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 3 --input_data_file 'data/asap_03/df_spell.csv' --input_splits_file 'data/asap_03/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 4 --input_data_file 'data/asap_04/df_spell.csv' --input_splits_file 'data/asap_04/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 5 --input_data_file 'data/asap_05/df_spell.csv' --input_splits_file 'data/asap_05/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 6 --input_data_file 'data/asap_06/df_spell.csv' --input_splits_file 'data/asap_06/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 7 --input_data_file 'data/asap_07/df_spell.csv' --input_splits_file 'data/asap_07/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 8 --input_data_file 'data/asap_08/df_spell.csv' --input_splits_file 'data/asap_08/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 9 --input_data_file 'data/asap_09/df_spell.csv' --input_splits_file 'data/asap_09/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 10 --input_data_file 'data/asap_10/df_spell.csv' --input_splits_file 'data/asap_10/splits/cv1.pkl'
+python3.9 train_auto_sas_.py --essay_set 1 --input_data_file 'data/asap_01/df_spell.csv' --input_splits_file 'data/asap_01/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 2 --input_data_file 'data/asap_02/df_spell.csv' --input_splits_file 'data/asap_02/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 3 --input_data_file 'data/asap_03/df_spell.csv' --input_splits_file 'data/asap_03/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 4 --input_data_file 'data/asap_04/df_spell.csv' --input_splits_file 'data/asap_04/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 5 --input_data_file 'data/asap_05/df_spell.csv' --input_splits_file 'data/asap_05/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 6 --input_data_file 'data/asap_06/df_spell.csv' --input_splits_file 'data/asap_06/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 7 --input_data_file 'data/asap_07/df_spell.csv' --input_splits_file 'data/asap_07/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 8 --input_data_file 'data/asap_08/df_spell.csv' --input_splits_file 'data/asap_08/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 9 --input_data_file 'data/asap_09/df_spell.csv' --input_splits_file 'data/asap_09/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 10 --input_data_file 'data/asap_10/df_spell.csv' --input_splits_file 'data/asap_10/splits/cv1.pkl' --use_better_spelling false
+python3.9 train_auto_sas_.py --essay_set 1 --input_data_file 'data/asap_01/df_spell.csv' --input_splits_file 'data/asap_01/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 2 --input_data_file 'data/asap_02/df_spell.csv' --input_splits_file 'data/asap_02/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 3 --input_data_file 'data/asap_03/df_spell.csv' --input_splits_file 'data/asap_03/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 4 --input_data_file 'data/asap_04/df_spell.csv' --input_splits_file 'data/asap_04/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 5 --input_data_file 'data/asap_05/df_spell.csv' --input_splits_file 'data/asap_05/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 6 --input_data_file 'data/asap_06/df_spell.csv' --input_splits_file 'data/asap_06/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 7 --input_data_file 'data/asap_07/df_spell.csv' --input_splits_file 'data/asap_07/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 8 --input_data_file 'data/asap_08/df_spell.csv' --input_splits_file 'data/asap_08/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 9 --input_data_file 'data/asap_09/df_spell.csv' --input_splits_file 'data/asap_09/splits/cv1.pkl' --augment_dataset false
+python3.9 train_auto_sas_.py --essay_set 10 --input_data_file 'data/asap_10/df_spell.csv' --input_splits_file 'data/asap_10/splits/cv1.pkl' --augment_dataset false
